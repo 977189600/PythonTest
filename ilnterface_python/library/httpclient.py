714c844586bdfa8def4f487f3fc956011efd4428
毕鹏飞 <bipengfei_ios@163.com>
2021-08-25 22:13:18 +0800

    jenkins 接口自动化部署

---
+++ ilnterface_python/library/httpclient.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+"""
+@Time:2021/8/23 10:11 下午"
+@Author:lydia_liu"
+@File:httpclient.py
+@function:
+"""
+import requests
+import urllib3
+
+
+class HttpClient:
+    """Generic Http Client class"""
+
+    def __init__(self, disable_ssl_verify=False, timeout=60):
+        """Initialize method"""
+
+        self.client = requests.session()
+        #无条件信任，不校验ssl
+        self.disable_ssl_verify = disable_ssl_verify
+        self.timeout = timeout
+        if self.disable_ssl_verify:
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    def Get(self, url, headers=None, data=None, json=None, params=None, *args, **kwargs):
+        """Http get method"""
+
+        if headers is None:
+            headers = {}
+
+        if self.disable_ssl_verify:
+            response = self.client.get(url, headers=headers, data=data, json=json, params=params
+                                       , verify=False, timeout=self.timeout, *args, **kwargs)
+        else:
+            response = self.client.get(url, headers=headers, data=data, json=json, params=params
+                                       , timeout=self.timeout, *args, **kwargs)
+        response.encoding = 'utf-8'
+        print(f'{response.json()}')
+
+        return response
